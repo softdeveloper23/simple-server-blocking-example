7aa4c83d6fe5f974f44adf8c2bae58fa44e03c1a
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-26 14:30:43 +0100

    Updated code

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Building A Simple Blocking Server
+// !!! - Make sure to Disable cache in Chrome Dev Tools - !!!
 
 const express = require('express');
 const cluster = require('cluster');
+const os = require('os');
+
+cluster.schedulingPolicy = cluster.SCHED_RR; // Forces Round Robin scheduling on Windows
 
 const app = express();
 
@@ -13,18 +17,21 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-    res.send('Performance example');
+    res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
     delay(9000);
-    res.send('Ding ding ding!');
+    res.send(`Ding ding ding! ${process.pid}`);
 });
 
-if (cluster.isMaster) {
+console.log('Running server.js...');
+if (cluster.isPrimary) {
     console.log(`Master has been started with pid ${process.pid}`);
-    cluster.fork();
-    cluster.fork();
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; i < NUM_WORKERS; i++) {
+        cluster.fork();
+    }
 } else {
     console.log(`Worker process started with pid ${process.pid}`);
     app.listen(3000);
